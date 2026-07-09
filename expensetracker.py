bd802c0dbfeca977e432f667e21293bb737190df
Athrvkushwah <lightingmacqueen@gamil.com>
2026-07-09 18:06:07 +0530

    second last commit

--- expensetracker.py
+++ expensetracker.py
@@ -1,13 +1,15 @@
 # #expense tracker 
-from Expenseediting import edit
-Add_expense = int(input("Enter your expense :", ))
-Add_catagrory = input('Catageory :')
-Reasone = input('Reasone :')
-print('Expense diary')
-print('Amount', Add_expense,  'Category:', Add_catagrory, 'Reason:', Reasone, sep = '\n')
+# from Expenseediting import edit
+def category1():
+  Add_expense = int(input("Enter your expense :", ))
+  Add_catagrory = input('Catageory :')
+  Reasone = input('Reasone :')
+  print('Expense diary')
+  print('Amount', Add_expense,  'Category:', Add_catagrory, 'Reason:', Reasone, sep = '\n')
+category1()
 
-Total_expense = Add_expense
-while True:
+def category2():
+ while True:
     while True:
       New_catageory = input('New catageory:')
       if not New_catageory:
@@ -24,12 +26,20 @@ while True:
         New_expense = int(input('Enter the new expense :'))
         if not New_expense:
           print('pls enter new expense')
-          break
-        Total_expense += New_expense
-        print("Total expense :",Total_expense)
         break
+    print('Amount :', New_expense , 'Reasone' ,New_Reasone , 'Category', New_catageory ,sep= '\n')
     break
+category2()
 
+def edit():
+    print('Select which catagory you want to edit')
+    while True:
+        print('1. cataogry1')
+        print('2  .catrgory2')
+        break
+    Option = input("choose the option :")
+    if Option == '1':category1()
+    elif Option =='2':category2()
 
 while True:
  print('1. yes')
@@ -38,3 +48,5 @@ while True:
 Option = input('If you want to edit the entry :')
 if Option =='1':edit()
 elif Option =='2':print('proceed')
+
+
